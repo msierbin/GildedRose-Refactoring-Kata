679b81ab9b84c05f66779faa82934453dcd878ed
Marek <msierbin@gmail.com>
2024-06-26 21:31:19 +0200

    Combine quality updates under singular condition tree

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,28 +5,29 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (item in items) {
             if (item.name == "Aged Brie") {
-                item.updateQualityBy(1)
                 if (item.sellInDays < 1) {
+                    item.updateQualityBy(2)
+                } else {
                     item.updateQualityBy(1)
                 }
                 item.updateSellInDaysBy(-1)
             } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                item.updateQualityBy(1)
-                if (item.sellInDays < 11) {
-                    item.updateQualityBy(1)
-                }
-                if (item.sellInDays < 6) {
-                    item.updateQualityBy(1)
-                }
                 if (item.sellInDays < 1) {
                     item.updateQualityBy(-item.quality)
+                } else if (item.sellInDays < 6) {
+                    item.updateQualityBy(3)
+                } else if (item.sellInDays < 11) {
+                    item.updateQualityBy(2)
+                } else {
+                    item.updateQualityBy(1)
                 }
                 item.updateSellInDaysBy(-1)
             } else if (item.name == "Sulfuras, Hand of Ragnaros") {
-                // empty
+                item.updateSellInDaysBy(0)
             } else {
-                item.updateQualityBy(-1)
                 if (item.sellInDays < 1) {
+                    item.updateQualityBy(-2)
+                } else {
                     item.updateQualityBy(-1)
                 }
                 item.updateSellInDaysBy(-1)
@@ -40,9 +41,7 @@ class GildedRose(var items: List<Item>) {
 //- The `Quality` of an item is never more than `50`
 private fun Item.updateQualityBy(increment: Int) {
     val newQuality = this.quality + increment
-    if (newQuality in 0..50) {
-        this.quality = newQuality
-    }
+    this.quality = newQuality.coerceIn(minimumValue = 0, maximumValue = 50)
 }
 
 private fun Item.updateSellInDaysBy(increment: Int) {
