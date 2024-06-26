4926d3ebd5e5ac5ba7ac1901dee19101529ad11c
Marek <msierbin@gmail.com>
2024-06-26 19:37:55 +0200

    Simplify assignements

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,21 +6,21 @@ class GildedRose(var items: List<Item>) {
         for (item in items) {
             if (item.name == "Aged Brie") {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
                 }
             } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
 
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                            item.quality += 1
                         }
                     }
 
                     if (item.sellIn < 6) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                            item.quality += 1
                         }
                     }
                 }
@@ -29,7 +29,7 @@ class GildedRose(var items: List<Item>) {
                     if (item.name == "Sulfuras, Hand of Ragnaros") {
                         // empty
                     } else {
-                        item.quality = item.quality - 1
+                        item.quality -= 1
                     }
                 }
             }
@@ -37,23 +37,23 @@ class GildedRose(var items: List<Item>) {
             if (item.name == "Sulfuras, Hand of Ragnaros") {
                 // empty
             } else {
-                item.sellIn = item.sellIn - 1
+                item.sellIn -= 1
             }
 
             if (item.sellIn < 0) {
                 if (item.name == "Aged Brie") {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality += 1
                     }
                 } else {
                     if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                     } else {
                         if (item.quality > 0) {
                             if (item.name == "Sulfuras, Hand of Ragnaros") {
                                 // empty
                             } else {
-                                item.quality = item.quality - 1
+                                item.quality -= 1
                             }
                         }
                     }
