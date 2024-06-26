f45bac60f8cdca3a10a4d2345139eb1cc9960829
Marek <msierbin@gmail.com>
2024-06-26 21:13:22 +0200

    Simplifications based on the assertions from quality

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,22 +5,18 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (item in items) {
             if (item.name == "Aged Brie") {
-                if (item.quality < 50) {
-                    item.updateQuality(1)
-                }
-                if (item.quality < 50 && item.sellIn < 1) {
+                item.updateQuality(1)
+                if (item.sellIn < 1) {
                     item.updateQuality(1)
                 }
                 item.updateSellIn()
             } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality < 50) {
+                item.updateQuality(1)
+                if (item.sellIn < 11) {
                     item.updateQuality(1)
-                    if (item.quality < 50 && item.sellIn < 11) {
-                        item.updateQuality(1)
-                    }
-                    if (item.quality < 50 && item.sellIn < 6) {
-                        item.updateQuality(1)
-                    }
+                }
+                if (item.sellIn < 6) {
+                    item.updateQuality(1)
                 }
                 if (item.sellIn < 1) {
                     item.quality = 0
@@ -29,10 +25,8 @@ class GildedRose(var items: List<Item>) {
             } else if (item.name == "Sulfuras, Hand of Ragnaros") {
                 // empty
             } else {
-                if (item.quality > 0) {
-                    item.updateQuality(-1)
-                }
-                if (item.quality > 0 && item.sellIn < 1) {
+                item.updateQuality(-1)
+                if (item.sellIn < 1) {
                     item.updateQuality(-1)
                 }
                 item.updateSellIn()
