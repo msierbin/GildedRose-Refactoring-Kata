222275453fa0ed8cd2bf1eb9527297a045c5bf49
Marek <msierbin@gmail.com>
2024-06-26 19:35:36 +0200

    Inverted all to == instead of !=. I want to see specific use cases handling, and else as a "default" for other use cases.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,13 +4,7 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
-                    }
-                }
-            } else {
+            if (item.name == "Aged Brie") {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
 
@@ -28,26 +22,56 @@ class GildedRose(var items: List<Item>) {
                         }
                     }
                 }
+            } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (item.quality > 0) {
+                    if (item.name == "Sulfuras, Hand of Ragnaros") {
+                        // empty
+                    } else {
+                        item.quality = item.quality - 1
+                    }
+                }
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
+            if (item.name == "Sulfuras, Hand of Ragnaros") {
+                // empty
+            } else {
                 item.sellIn = item.sellIn - 1
             }
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.name == "Aged Brie") {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                } else {
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        item.quality = item.quality - item.quality
+                    } else {
                         if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            if (item.name == "Sulfuras, Hand of Ragnaros") {
+                                // empty
+                            } else {
                                 item.quality = item.quality - 1
                             }
                         }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
                     }
                 }
             }
