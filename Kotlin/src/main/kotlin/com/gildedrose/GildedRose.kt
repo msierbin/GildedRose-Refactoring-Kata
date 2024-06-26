57b19d06018beb91e52ed0bf917c3e8545722098
Marek <msierbin@gmail.com>
2024-06-26 21:35:37 +0200

    Change method to be on top

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,31 +5,32 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (item in items) {
             if (item.name == "Aged Brie") {
-                if (item.sellInDays < 1) {
-                    item.updateQualityBy(2)
-                } else {
-                    item.updateQualityBy(1)
-                }
+                item.updateQualityBy(
+                    when {
+                        item.sellInDays < 1 -> 2
+                        else -> 1
+                    }
+                )
                 item.updateSellInDaysBy(-1)
             } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.sellInDays < 1) {
-                    item.updateQualityBy(-item.quality)
-                } else if (item.sellInDays < 6) {
-                    item.updateQualityBy(3)
-                } else if (item.sellInDays < 11) {
-                    item.updateQualityBy(2)
-                } else {
-                    item.updateQualityBy(1)
-                }
+                item.updateQualityBy(
+                    when {
+                        item.sellInDays < 1 -> -item.quality
+                        item.sellInDays < 6 -> 3
+                        item.sellInDays < 11 -> 2
+                        else -> 1
+                    }
+                )
                 item.updateSellInDaysBy(-1)
             } else if (item.name == "Sulfuras, Hand of Ragnaros") {
                 item.updateSellInDaysBy(0)
             } else {
-                if (item.sellInDays < 1) {
-                    item.updateQualityBy(-2)
-                } else {
-                    item.updateQualityBy(-1)
-                }
+                item.updateQualityBy(
+                    when {
+                        item.sellInDays < 1 -> -2
+                        else -> -1
+                    }
+                )
                 item.updateSellInDaysBy(-1)
             }
         }
