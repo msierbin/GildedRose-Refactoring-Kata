6e8347ed2172144766e41e29bee40c77f4163738
Marek <msierbin@gmail.com>
2024-06-26 21:14:57 +0200

    Renamed for clarity

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,18 +5,18 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (item in items) {
             if (item.name == "Aged Brie") {
-                item.updateQuality(1)
+                item.updateQualityBy(1)
                 if (item.sellIn < 1) {
-                    item.updateQuality(1)
+                    item.updateQualityBy(1)
                 }
                 item.updateSellIn()
             } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                item.updateQuality(1)
+                item.updateQualityBy(1)
                 if (item.sellIn < 11) {
-                    item.updateQuality(1)
+                    item.updateQualityBy(1)
                 }
                 if (item.sellIn < 6) {
-                    item.updateQuality(1)
+                    item.updateQualityBy(1)
                 }
                 if (item.sellIn < 1) {
                     item.quality = 0
@@ -25,9 +25,9 @@ class GildedRose(var items: List<Item>) {
             } else if (item.name == "Sulfuras, Hand of Ragnaros") {
                 // empty
             } else {
-                item.updateQuality(-1)
+                item.updateQualityBy(-1)
                 if (item.sellIn < 1) {
-                    item.updateQuality(-1)
+                    item.updateQualityBy(-1)
                 }
                 item.updateSellIn()
             }
@@ -38,7 +38,7 @@ class GildedRose(var items: List<Item>) {
 
 //- The `Quality` of an item is never negative
 //- The `Quality` of an item is never more than `50`
-private fun Item.updateQuality(increment: Int) {
+private fun Item.updateQualityBy(increment: Int) {
     val newQuality = this.quality + increment
     if (newQuality in 0..50) {
         this.quality = newQuality
