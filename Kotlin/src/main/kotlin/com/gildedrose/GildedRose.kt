2c6fd701afd75d7a93b1bd078771f3d6a974de53
Marek <msierbin@gmail.com>
2024-06-26 21:06:56 +0200

    Extract quality method

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,20 +6,20 @@ class GildedRose(var items: List<Item>) {
         for (item in items) {
             if (item.name == "Aged Brie") {
                 if (item.quality < 50) {
-                    item.quality += 1
+                    item.updateQuality(1)
                 }
                 if (item.quality < 50 && item.sellIn < 1) {
-                    item.quality += 1
+                    item.updateQuality(1)
                 }
                 item.updateSellIn()
             } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality < 50) {
-                    item.quality += 1
+                    item.updateQuality(1)
                     if (item.quality < 50 && item.sellIn < 11) {
-                        item.quality += 1
+                        item.updateQuality(1)
                     }
                     if (item.quality < 50 && item.sellIn < 6) {
-                        item.quality += 1
+                        item.updateQuality(1)
                     }
                 }
                 if (item.sellIn < 1) {
@@ -30,16 +30,20 @@ class GildedRose(var items: List<Item>) {
                 // empty
             } else {
                 if (item.quality > 0) {
-                    item.quality -= 1
+                    item.updateQuality(-1)
                 }
                 if (item.quality > 0 && item.sellIn < 1) {
-                    item.quality -= 1
+                    item.updateQuality(-1)
                 }
                 item.updateSellIn()
             }
         }
     }
 
+}
+
+private fun Item.updateQuality(int: Int) {
+    this.quality += int
 }
 
 private fun Item.updateSellIn() {
